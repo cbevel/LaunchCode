0dbc1ead3f716fb9a664b22a36290f0d1f6b31e5
Chels Noel <chelseybevel@Chelseys-MacBook-Air.local>
2020-04-19 19:22:21 -0400

    more changs to rover class

--- practiceAssign3.js
+++ practiceAssign3.js
@@ -25,18 +25,19 @@ class Rover {
       this.mode = mode;
       this.generatorWatts = generatorWatts;
       }
+
     receiveMessage(message) {
-     return message;        
+      let Message = {};
+      
   }
 }
 
-  let commands = [new Command('MODE_CHANGE'), new Command('STATUS_CHECK')];
-  let message = new Message('Test message with two commands', commands);  
-  let rover = new Rover(2000);
+let commands = [new Command('MODE_CHANGE', 'LOW_POWER'), new Command('STATUS_CHECK')];
+let message = new Message('Test message with two commands', commands);
+let rover = new Rover(98382);    // Passes 98382 as the rover's position.
+let response = rover.receiveMessage(message);
 
+console.log(response);
 
-
-
-  rover.receiveMessage()
 
 
