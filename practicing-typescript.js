b58a1f8fe2559ad6d5eea1ad11b748a01bb0f2b3
Chels Noel <chelseybevel@Chelseys-MacBook-Air.local>
2020-05-18 21:57:52 -0400

    printing first message

---
+++ practicing-typescript.js
@@ -0,0 +1,6 @@
+var spaceShuttleName = "Determination";
+var shuttleSpeed = 17500;
+var distancetoMars = 225000000;
+var distancetoMoon = 384400;
+var milesperKilometer = 0.621;
+console.log(spaceShuttleName);
