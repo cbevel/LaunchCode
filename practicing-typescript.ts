f4eea45fd05c7c655a74b977d60306d90db9bbe5
Chels Noel <chelseybevel@Chelseys-MacBook-Air.local>
2020-05-18 21:13:29 -0400

    init

---
+++ practicing-typescript.ts
@@ -0,0 +1,7 @@
+let spaceShuttleName: string = "Determination";
+let shuttleSpeed: number = 17500;
+let distancetoMars: number = 225000000;
+let distancetoMoon: number = 384400;
+let milesperKilometer: number = 0.621;
+
+console.log(spaceShuttleName);
